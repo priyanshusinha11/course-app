110ed7f35e4fff7304bdca579f0321684017f065
Priyanshu Sinha <priyanshusinha.2807@gmail.com>
2023-08-16 21:48:21 +0530

    wrote the addcourse and signup page, connected them to the backend

--- frontend/src/AddCourse.jsx
+++ frontend/src/AddCourse.jsx
@@ -1,15 +1,61 @@
 import Button from '@mui/material/Button';
 import TextField from "@mui/material/TextField";
 import { Card, Typography } from "@mui/material";
+import { useState } from "react";
 
 function AddCourse() {
-    return <div>
-        <div>
-            <Typography>
-                Add a course below.
-            </Typography>
-        </div>
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    return <div style={{ display: "flex", justifyContent: "center" }}>
+        <Card variant={'outlined'} style={{ width: 400, padding: 20 }}>
+            <TextField
+                onChange={(e) => {
+                    setTitle(e.target.value);
+                }}
+                fullWidth={true}
+                label="Title"
+                variant='outlined'
+            />
+            <br /><br />
+            <TextField
+                onChange={(e) => {
+                    setDescription(e.target.value);
+                }}
+                fullWidth={true}
+                label="Description"
+                variant='outlined'
+            />
+            <br /><br />
+            <Button
+                size={"large"}
+                variant='contained'
+                onClick={() => {
+                    function callback2(data) {
+                        localStorage.setItem("token", data.token);
+                    }
+                    function callback1(res) {
+                        res.json().then(callback2);
+                    }
+                    fetch("http://localhost:3000/admin/courses", {
+                        method: "POST",
+                        body: JSON.stringify({
+                            title: title,
+                            description: description,
+                            imageLink: "",
+                            published: true
+                        }),
+                        headers: {
+                            "Content-type": "application/json",
+                            "Authorization": "Bearer " + localStorage.getItem("token")
+                        }
+                    }).then(callback1)
+                }}
+            >Add Course</Button>
+
+        </Card>
+
     </div>
+
 }
 
 export default AddCourse;
