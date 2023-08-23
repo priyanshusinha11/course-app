3a240bb6635d4043c9026baa04d325ce4ba626f1
Priyanshu <priyanshusinha.2807@gmail.com>
2023-08-23 10:41:59 +0530

    added the course and courses pageand included recoil

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,15 @@ import Signin from "./Signin.jsx";
 import Signup from "./Signup.jsx";
 import Appbar from "./Appbar.jsx";
 import AddCourse from "./AddCourse.jsx";
+import Courses from "./Courses.jsx";
+import Course from "./Course.jsx";
+import {
+    RecoilRoot,
+    atom,
+    selector,
+    useRecoilState,
+    useRecoilValue,
+} from 'recoil'
 
 function App() {
 
@@ -13,15 +22,20 @@ function App() {
             backgroundColor: "#eeeeee"
         }}
         >
-            <Router>
-                <Appbar />
-                <Routes>
-                    <Route path={"/addcourse"} element={
-                        <AddCourse />} />
-                    <Route path={"/signin"} element={<Signin />} />
-                    <Route path={"/signup"} element={<Signup />} />
-                </Routes>
-            </Router>
+            <RecoilRoot>
+                <Router>
+                    <Appbar />
+                    <Routes>
+                        <Route path={"/addcourse"} element={
+                            <AddCourse />} />
+                        <Route path={"/signin"} element={<Signin />} />
+                        <Route path={"/signup"} element={<Signup />} />
+                        <Route path={"/course/:courseId"} element={<Course />} />
+                        <Route path={"/courses"} element={<Courses />} />
+                    </Routes>
+                </Router>
+            </RecoilRoot>
+
         </div>
     );
 }
